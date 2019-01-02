14ebd29ebf4f04326061f532c6db148437d8c348
Brandon Redshirt <brandon.redshirt@gmail.com>
2019-01-02 11:40:46 -0700

    init home and index pages

---
+++ src/Components/IndexPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => {
+    return (
+        <div className='container'>
+            <h1>IndexPage</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum mattis ligula, ut porta magna interdum vitae. Quisque faucibus lacus sit amet metus fringilla, non posuere mauris vestibulum. Nullam ac massa eu lacus tempor vulputate. Donec congue a ligula quis tincidunt. Nulla imperdiet mi quis nunc convallis, eu lacinia est semper. Nam eget metus sed sem feugiat gravida. In hac habitasse platea dictumst. Sed lobortis egestas accumsan. Maecenas semper ex non massa vehicula, sit amet blandit eros tempus. Cras cursus dui nec arcu ornare dictum. Duis ut urna mauris. Nullam rutrum vitae velit non aliquam. Cras nec urna molestie, maximus dui id, ornare ipsum. Praesent luctus sem egestas viverra elementum.</p>
+        </div>
+    )
+}
+
+export default IndexPage
